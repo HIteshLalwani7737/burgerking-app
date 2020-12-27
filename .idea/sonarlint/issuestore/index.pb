d436ee27a783c04cb1e7d8bee63d5a13beffb65f
hitesh7737 <hitesh.lalwani@magicbricks.com>
2020-12-27 20:15:52 +0530

    This is a new commit for what I originally planned to be amended

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+<
+package.json,7\0\7030d0b2f71b999ff89a343de08c414af32fc93a
+>
+pages/index.js,1\7\17f8c719689e4f3221289153187a6ca1fb24dd87
+=
+pages/home.js,d\8\d831c02750cfbe83d7fcb2e3dfb8d63fb7bed976
---
+++ pages/home.js
@@ -0,0 +1,13 @@
+import React,{Component} from "React";
+
+class Home extends Component{
+    render(){
+return(
+    <div>
+        <h1>Hello Hitesh lalwani</h1>
+        </div>
+);
+
+    }
+}
+export default Home;
